eae092a0b231826b9664ebd40c740cbc715f7bee
Kaushal Parikh <kaushalp88@gmail.com>
2013-04-07 01:32:14 -0400

    basic back end stuff

---
+++ server.js
@@ -0,0 +1,13 @@
+var app = require('express')()
+    , server = require('http').createServer(app)
+    , io = require('socket.io') 
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/index.html')
+});
+
+app.get('/display', function(req, res){
+    res.sendfile(__dirname + '/display.html')
+});
+
+server.listen(3000);
