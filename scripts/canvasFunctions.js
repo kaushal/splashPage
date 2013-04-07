a9c522ed8e0b497769e190cee8aae87e3252af3d
Kaushal Parikh <kaushalp88@gmail.com>
2013-04-07 04:48:30 -0400

    communication between express and front end display

--- scripts/canvasFunctions.js
+++ scripts/canvasFunctions.js
@@ -3,9 +3,16 @@ $(document).ready(function(){
     var xPos;
     var yPos;
     $displayCanvas.click(function(e){
+        var socket = io.connect('/')
+
+
         var offset = $(this).offset();
         var xPos = e.clientX;
         var yPos = e.clientY;
+        
+        socket.emit('xPos', xPos);
+        socket.emit('yPos', yPos);
+
         console.log("x: " + xPos + "y: " + yPos);
     });
 });
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var app = require('express')()
+var express = require('express')
+    , app = express()
     , server = require('http').createServer(app)
-    , io = require('socket.io')
+    , io = require('socket.io').listen(server)
+    , xPos = 0
+    , yPos = 0
 
+app.use(express.static('scripts'))
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/phone.html')
 });
@@ -10,4 +14,16 @@ app.get('/display', function(req, res){
     res.sendfile(__dirname + '/display.html')
 });
 
+
+
+io.sockets.on('connection', function(socket){
+   socket.on('xPos', function(value) {
+       xPos = value
+    console.log('setting x value: ' + value) 
+   });
+   socket.on('yPos', function(value){
+        ypos = value;
+        console.log('setting y value: ' + value)
+   });
+});
 server.listen(3000);
