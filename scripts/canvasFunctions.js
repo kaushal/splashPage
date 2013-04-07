2ce38fbaed122725ab53fc93de532d4f331e5980
Sam Agnew <sagnew@eden.rutgers.edu>
2013-04-07 03:26:44 -0400

    Should get x and y positions

--- scripts/canvasFunctions.js
+++ scripts/canvasFunctions.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     $displayCanvas = $('#displayCanvas');
+    var xPos, yPos;
+    $displayCanvas.click(function(e){
+        var offset = $(this).offset();
+        var xPos = e.clientX;
+        var yPos = e.clientY;
+        alert("x: " + xPos + "y: " + yPos);
+    });
 });
